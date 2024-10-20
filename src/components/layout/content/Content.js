bf48db5960af09cac4427fa266beabb892145656
seojung <6dreamhigh@naver.com>
2024-10-20 16:37:59 +0900

    💄 STYLE: 컨텐츠 영역에 프로필 공간 추가

--- src/components/layout/content/Content.js
+++ src/components/layout/content/Content.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Content.css'; // Content 관련 CSS 파일 분리
 import { FaBars } from "react-icons/fa6";
+import { IoPersonCircle } from "react-icons/io5";
 
 const Content = ({ isNavOpen, toggleNav, effectClass }) => {
   return (
@@ -8,7 +9,18 @@ const Content = ({ isNavOpen, toggleNav, effectClass }) => {
       <button className={`nav-open-btn ${isNavOpen ? 'js-hidden' : ''}`} onClick={toggleNav}>
         <FaBars/>
       </button>
+      
+      {/* 프로필 및 환영 메시지 추가 */}
+      <div className="profile-section">
+        <div className="profile-info">
+          <IoPersonCircle className='profile-img'/>
+          <span className="welcome-text">관리자님 환영합니다</span>
+        </div>
+        
+      </div>
+      <hr className="divider" />
       <div className="content">
+        <h1>컨텐츠 공간</h1>
       </div>
     </div>
   );
