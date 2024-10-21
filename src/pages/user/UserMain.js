0009768d2b6b9171b65f1dd35ade8f4026095aa2
seojung <6dreamhigh@naver.com>
2024-10-21 17:59:01 +0900

    🔥 REMOVE: UserMain 필요없는 함수 및 변수 삭제

--- src/pages/user/UserMain.js
+++ src/pages/user/UserMain.js
@@ -1,21 +1,8 @@
 import React, { useState } from "react";
 import "./UserMain.css";
 import { BsTable } from "react-icons/bs";
-import { FiPieChart } from "react-icons/fi";
-import DateSelectBar from "./dropdown/DateSelectBar";
 
 const UserMain = () => {
-  const [selectedOption, setSelectedOption] = useState("전체");
-  const [activeButton, setActiveButton] = useState(null);
-
-  const handleOptionChange = (event) => {
-    setSelectedOption(event.target.value);
-  };
-
-  const handleButtonClick = (button) => {
-    setActiveButton(button); // 클릭한 버튼을 활성화 상태로 설정
-  };
-
   return (
     <div className="headerContainer">
       <div className="content">
