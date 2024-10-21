75e53f28731254789743385f926c96b02b9bec8a
hamin-yeong <mytokki0323@gmail.com>
2024-10-21 14:59:44 +0900

    💄 STYLE: font적용

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "./assets/fonts/paybooc_font/Font.css";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
