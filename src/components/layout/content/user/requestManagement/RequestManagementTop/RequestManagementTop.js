eb97438e53093ac9cce35eda633a2fb68a25ae22
seojung <6dreamhigh@naver.com>
2024-10-24 10:40:28 +0900

    💄 STYLE: font 사이즈 축소

--- src/components/layout/content/user/requestManagement/RequestManagementTop/RequestManagementTop.js
+++ src/components/layout/content/user/requestManagement/RequestManagementTop/RequestManagementTop.js
@@ -39,7 +39,7 @@ const RequestSelectTop = () => {
             <h3>
               <span className="number">15</span> 건
             </h3>
-            <span>총 건수</span>
+            <span>처리 중</span>
           </div>
         </div>
       </div>
@@ -51,7 +51,7 @@ const RequestSelectTop = () => {
             <h3>
               <span className="number">15</span> 건
             </h3>
-            <span>총 건수</span>
+            <span>처리 완료</span>
           </div>
         </div>
       </div>
