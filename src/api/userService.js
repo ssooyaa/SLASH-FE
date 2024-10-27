723bed12f298f3f2093c6fa3686da22704d13464
김수지 <tvfxq0214@gmail.com>
2024-10-28 00:07:42 +0900

    💄  STYLE: 파일명 변경

--- src/api/userService.js
+++ src/api/userService.js
@@ -1,4 +1,3 @@
-// 사용자 관련 API 호출 함수 모음 (예: 사용자 정보 가져오기)
 import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:8080";
--- src/components/layout/content/contractManager/contract/contractForm/ContractForm.js
+++ src/components/layout/content/contractManager/contract/contractForm/ContractForm.js
@@ -4,7 +4,7 @@ import ContractHeader from "../contractHeader/ContractHeader";
 import ContractStep1 from "../contractContent/ContractStep1";
 import ContractStep2 from "../contractContent/ContractStep2";
 import ContractCheck from "../contractContent/ContractCheck";
-import { CreateContract } from "../../../../../../api/userService";
+import { CreateContract } from "../../../../../../api/UserService";
 
 const ContractForm = () => {
   const steps = [
