92c7b6ec585006980d29d07d19b5f277dc7e15b9
김수지 <tvfxq0214@gmail.com>
2024-10-30 02:00:44 +0900

    ✨ FEAT: 평가점수 가로형 테이블 및 라벨 컴포넌트 구현

---
+++ src/components/feature/table/GradeHorizonTable.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const GradeHorizonTable = ({ initialData }) => {
+  const data = initialData ? initialData : [{ min: 1 }, { min: 2 }];
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>점수 평가</th>
+            {data.map((item, index) => (
+              <th key={index}>
+                {item.min}
+                {item.minInclusive ? "이상" : "초과"} ~ {item.max}
+                {item.maxInclusive ? "이하" : "미만"}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>평가 등급</td>
+            {data.map((item, index) => (
+              <td key={index}>{item.grade}</td>
+            ))}
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+};
+export default GradeHorizonTable;
