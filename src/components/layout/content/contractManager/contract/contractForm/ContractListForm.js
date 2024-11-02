6bd551edb5331571e8956ce0ee553b7c4f627e98
김수지 <tvfxq0214@gmail.com>
2024-11-02 21:03:59 +0900

    ✨ FEAT: 전체 계약 리스트 페이지 구현

---
+++ src/components/layout/content/contractManager/contract/contractForm/ContractListForm.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import "./ContractListForm.css";
+import { fetchAllContractInfo } from "../../../../../../api/UserService";
+import ContractListTable from "../../../../../feature/table/ContractListTable";
+
+const ContractListForm = () => {
+  const [formData, setFormData] = useState([]);
+  useEffect(() => {
+    const loadData = async () => {
+      const response = await fetchAllContractInfo();
+      console.log(response);
+      setFormData(response);
+    };
+    loadData();
+  }, []);
+  return (
+    <>
+      <div className="contractListForm">
+        <div className="contractListInfo">
+          <div className="ContractTitle">
+            <p>계약 현황 조회</p>
+          </div>
+          <div className="table contractListTable">
+            <ContractListTable initialData={formData} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default ContractListForm;
