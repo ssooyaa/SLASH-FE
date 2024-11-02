c656f6a81eedf82cf80c12394de9e82dde9adb49
김수지 <tvfxq0214@gmail.com>
2024-11-02 21:04:48 +0900

    ✨ FEAT: 율(%) unit 타입 서비스 변환 점수 없는 테이블 컴포넌트 구현

---
+++ src/components/feature/table/ContractListTable.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const ContractListTable = ({ initialData = [] }) => {
+  const data = initialData;
+  const navigator = useNavigate();
+
+  const handleDetail = (contractId) => {
+    navigator(`/contractManager/contract/${contractId}`);
+  };
+
+  const handleEdit = (contractId) => {
+    navigator(`/contractManager/updateContract/${contractId}`);
+  };
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>회사명</th>
+            <th>시작일</th>
+            <th>종료일</th>
+            <th>상세보기</th>
+            <th>수정하기</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>{item.companyName}</td>
+              <td>{item.startDate}</td>
+              <td>{item.endDate}</td>
+              <td>
+                <button onClick={() => handleDetail(item.contractId)}>
+                  상세보기
+                </button>
+              </td>
+              <td>
+                {item.terminate ? null : (
+                  <button onClick={() => handleEdit(item.contractId)}>
+                    수정하기
+                  </button>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default ContractListTable;
