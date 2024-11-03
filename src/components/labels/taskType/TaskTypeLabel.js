a2e031f517f200bd2caaa781ff8bc03bfc4329cd
seojung <6dreamhigh@naver.com>
2024-11-03 13:20:57 +0900

    💄 STYLE : 요청 유형 라벨 CSS 수정

--- src/components/labels/taskType/TaskTypeLabel.js
+++ src/components/labels/taskType/TaskTypeLabel.js
@@ -4,9 +4,9 @@ const TaskTypeLabel = ({ taskType }) => {
   const getLabelStyle = (taskType) => {
     switch (taskType) {
       case "서비스 요청":
-        return { backgroundColor: "#ffefef", color: "#ff4d4f" };
+        return { backgroundColor: "#F6E9FF", color: "#C66FFC" };
       case "장애 요청":
-        return { backgroundColor: "#FFE4DC", color: "#FE4853" };
+        return { backgroundColor: "#f0ffff", color: "#008080" };
       case "기타":
         return { backgroundColor: "#f0f0f0", color: "#8c8c8c" };
     }
