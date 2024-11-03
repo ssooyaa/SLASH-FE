26d8027b58ce871899edfb0034d427027cd7a8e9
hamin-yeong <mytokki0323@gmail.com>
2024-11-04 00:36:44 +0900

    :sparkles: FEAT: 월간 통계 데이터 가져오는 API연결

--- src/api/UserService.js
+++ src/api/UserService.js
@@ -29,3 +29,18 @@ export const CreateServiceDetail = async (requestContractDTO) => {
     return false;
   }
 };
+
+export const getMonthlyData = async (selectedYear, selectedMonth) => {
+  try {
+    const params = {year: selectedYear, month: selectedMonth};
+    console.log("전송 값:", params);
+
+    const response = await axios.get("/monthly-data", {params});
+    console.log("서버 응답:", JSON.stringify(response.data, null, 2));
+
+    return response.data; // 데이터를 반환
+  } catch (error) {
+    console.error("데이터 전송 오류:", error);
+    return null; // 오류 발생 시 null 반환
+  }
+};
