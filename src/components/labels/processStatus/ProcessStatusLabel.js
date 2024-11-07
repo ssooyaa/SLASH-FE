12c2b712c1350c79e5cd0caac0af1dad7a2ec17e
seojung <6dreamhigh@naver.com>
2024-11-07 17:06:53 +0900

    🐛 FIX: 접수하기 라벨-> 할당하기 버튼으로 수정

--- src/components/labels/processStatus/ProcessStatusLabel.js
+++ src/components/labels/processStatus/ProcessStatusLabel.js
@@ -1,26 +1,33 @@
 import React from "react";
 import "../../../styles/Label.css";
+import AssignmentButton from "../../common/button/AssignmentButton";
+import { useNavigate } from "react-router-dom";
 
-const ProcessStatusLabel = ({ processType }) => {
-  // 스타일을 taskType에 따라 동적으로 설정
+const ProcessStatusLabel = ({ processType, id }) => {
+  const navigate = useNavigate();
   const getLabelStyle = (processType) => {
     switch (processType) {
-      case "REGISTERED":
-        return { backgroundColor: "#FFEFDC", color: "#FF9A16" };
       case "IN_PROGRESS":
         return { backgroundColor: "#C3DBF8", color: "#2C70F4" };
       case "COMPLETED":
         return { backgroundColor: "#C3F8DD", color: "#2e8b57" };
       default:
-        return { backgroundColor: "#fff", color: "#000" }; // 기본값: 흰색 배경, 검은색 글자
+        return { backgroundColor: "#fff", color: "#000" };
     }
   };
 
-  //표시할 텍스트를 processType에 따라 설정
   const getDisplayText = (processType) => {
     switch (processType) {
       case "REGISTERED":
-        return "접수 완료";
+        return (
+          <AssignmentButton
+            id={id}
+            onClick={() => {
+              console.log("ProcessStatusLabel에서 할당하기 클릭됨:", id);
+              navigate(`request/${id}`); // 처음 클릭 시 이동
+            }}
+          />
+        );
       case "IN_PROGRESS":
         return "진행 중";
       case "COMPLETED":
