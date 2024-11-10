f8a09edb4eaaa7b1a5dbfb5226c8541e73cbd6f2
seojung <6dreamhigh@naver.com>
2024-11-10 22:25:44 +0900

    ✨ FEAT: 종합지표 페이지 구현

---
+++ src/components/layout/content/user/indexManagement/MiddleIndex.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./MiddleIndex.css";
+
+const MiddleIndex = () => {
+  return (
+    <div className="middle">
+      <div className="totalIndex"> 종합평가 등급</div>
+      <div className="totalIndex"> 요청 건수</div>
+      <div className="totalIndex">장애 발생시간</div>
+    </div>
+  );
+};
+
+export default MiddleIndex;
