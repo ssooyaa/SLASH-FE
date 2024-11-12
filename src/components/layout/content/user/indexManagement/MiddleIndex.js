8059eab6666a89428d6f61cda50650e0b4168f84
seojung <6dreamhigh@naver.com>
2024-11-12 10:40:12 +0900

    ✨ FEAT: 장애 시간 포맷팅

--- src/components/layout/content/user/indexManagement/MiddleIndex.js
+++ src/components/layout/content/user/indexManagement/MiddleIndex.js
@@ -10,35 +10,42 @@ const MiddleIndex = ({ agreementId, date }) => {
     incidentTime: 0,
   });
 
+  const formatDowntimeToHours = (time) => {
+    if (time < 60) {
+      return `${time}m`;
+    } else {
+      const hours = Math.floor(time / 60);
+      const minutes = time % 60;
+      return minutes > 0 ? `${hours}h ${minutes}m` : `${hours}h`;
+    }
+  };
+
   useEffect(() => {
     const fetchData = async () => {
       if (agreementId && date) {
-        // agreementId와 date가 유효한 경우에만 요청
         try {
           const response = await fetchIndicators(agreementId, date);
-
           if (response && response.success) {
             const { grade, requestCount, incidentTime } =
               response.data.indicatorEtcInfo;
             setIndicatorData({ grade, requestCount, incidentTime });
           } else {
-            // 실패 시 기본값 설정
             setIndicatorData({ grade: "-", requestCount: 0, incidentTime: 0 });
           }
         } catch (error) {
           console.error("Failed to fetch indicator data:", error);
-          // 오류 발생 시 기본값 설정
           setIndicatorData({ grade: "-", requestCount: 0, incidentTime: 0 });
         }
       }
     };
     fetchData();
-  }, [agreementId, date]); // 의존성 배열에 agreementId와 date 추가
+  }, [agreementId, date]);
+
   return (
     <div className="middle">
       <div className="totalIndex">
         <div className="title">종합평가 등급</div>
-        <div className="value">{indicatorData.grade}</div>
+        <div className="value">{indicatorData.grade}등급</div>
         <div className="subText">{indicatorData.score}점</div>
       </div>
       <div className="totalIndex">
@@ -47,7 +54,9 @@ const MiddleIndex = ({ agreementId, date }) => {
       </div>
       <div className="totalIndex">
         <div className="title">장애 발생 시간</div>
-        <div className="value">{indicatorData.incidentTime}h</div>
+        <div className="value">
+          {formatDowntimeToHours(indicatorData.incidentTime)}
+        </div>
       </div>
     </div>
   );
