5aa61de193f88d569a311004cab443966625fe07
seojung <6dreamhigh@naver.com>
2024-11-17 23:10:48 +0900

    🐛 FIX: 요청관리자 API에 맞게 수정

--- src/components/layout/content/requestManager/RequestManagerMainContent.js
+++ src/components/layout/content/requestManager/RequestManagerMainContent.js
@@ -1,23 +1,24 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "../../../../styles/Content.css"; // 공통 Content css 파일 import
-import {FaBars} from "react-icons/fa6";
-import {IoPersonCircle} from "react-icons/io5";
+import { FaBars } from "react-icons/fa6";
+import { IoPersonCircle } from "react-icons/io5";
 import RequestManagerTop from "./requestManagerTop/RequestManagerTop";
 import RequestManagerBottom from "./requestManagerBottom/RequestManagerBottom";
 import ContractHeaderV2 from "../../../common/header/ContractHeaderV2";
-import {fetchContractInfo} from "../../../../api/CommonService";
+import { fetchContractInfo } from "../../../../api/CommonService";
 
-const RequestManagerMainContent = ({isNavOpen, toggleNav, effectClass}) => {
-  const [selectedAgreementId, setSelectedAgreementId] = useState(null);
+const RequestManagerMainContent = ({ isNavOpen, toggleNav, effectClass }) => {
+  const [selectedAgreementId, setSelectedAgreementId] = useState(
+    localStorage.getItem("selectedAgreementId") || null
+  );
   const [contractInfo, setContractInfo] = useState({});
 
-  // 콜백 함수 정의
-  const handleContractSelection = async (agreementId) => {
-    console.log("들어오니? ", agreementId);
-    const contractDataInfo = await fetchContractInfo(agreementId);
-    console.log(contractDataInfo)
-    setContractInfo(contractDataInfo);
+  //콜백 함수 정의
+  const handleContractSelection = (agreementId) => {
+    setSelectedAgreementId(agreementId);
 
+    //localStorage에 값 저장
+    localStorage.setItem("selectedAgreementId", agreementId);
   };
 
   useEffect(() => {
@@ -37,24 +38,26 @@ const RequestManagerMainContent = ({isNavOpen, toggleNav, effectClass}) => {
         className={`navOpenBtn ${isNavOpen ? "jsHidden" : ""}`}
         onClick={toggleNav}
       >
-        <FaBars/>
+        <FaBars />
       </button>
 
       {/* 프로필 및 환영 메시지 추가 */}
       <div className="profileSection">
         <div className="profileInfo">
-          <IoPersonCircle className="profileImg"/>
+          <IoPersonCircle className="profileImg" />
           <span className="welcomeText">관리자님 환영합니다</span>
         </div>
       </div>
-      <hr className="divider"/>
+      <hr className="divider" />
       <div className="content">
         <div className="contentBox">
-          <ContractHeaderV2 onContractSelect={handleContractSelection}
-                            setSelectedAgreementId={setSelectedAgreementId}
-                            selectedAgreementId={selectedAgreementId}/>
-          <RequestManagerTop contractId={selectedAgreementId}/>
-          <RequestManagerBottom/>
+          <ContractHeaderV2
+            onContractSelect={handleContractSelection}
+            setSelectedAgreementId={setSelectedAgreementId}
+            selectedAgreementId={selectedAgreementId}
+          />
+          <RequestManagerTop contractId={selectedAgreementId} />
+          <RequestManagerBottom agreementId={selectedAgreementId} />
         </div>
       </div>
     </div>
