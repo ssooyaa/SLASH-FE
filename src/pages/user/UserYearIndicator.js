36ac362b9725ecee2b20e378a2b23129fce07895
김수지 <tvfxq0214@gmail.com>
2024-11-17 22:19:07 +0900

    ✨ FEAT: 유저페이지 연간통계 사이드바 적용

---
+++ src/pages/user/UserYearIndicator.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import UserSidebar from "../../components/layout/sidebar/UserSidebar";
+import "../../styles/Content.css";
+import YearIndicatorInfo from "../../components/layout/content/contractManager/yearIndicator/YearIndicatorInfo";
+
+const UserYearIndicator = () => {
+  const [isNavOpen, setNavOpen] = useState(true);
+  const [effectClass, setEffectClass] = useState(1);
+
+  const toggleNav = () => {
+    setNavOpen(!isNavOpen);
+  };
+
+  //TODO: 헤더 추가
+
+  return (
+    <div>
+      <UserSidebar
+        isNavOpen={isNavOpen}
+        toggleNav={toggleNav}
+        effectClass={effectClass}
+        setEffectClass={setEffectClass}
+      />
+
+      <YearIndicatorInfo
+        isNavOpen={isNavOpen}
+        toggleNav={toggleNav}
+        effectClass={effectClass}
+      />
+    </div>
+  );
+};
+
+export default UserYearIndicator;
