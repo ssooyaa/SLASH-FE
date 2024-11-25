ddcf8722356cec8ab9d81ed6c1a5d8ef6589a204
김수지 <tvfxq0214@gmail.com>
2024-11-25 13:05:48 +0900

    ♻️ REFACTOR: 요청관리자 업무내역 공통 헤더 적용

--- src/components/common/header/ContentsHeader.js
+++ src/components/common/header/ContentsHeader.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { FaAsterisk, FaRegCalendar } from "react-icons/fa6";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
