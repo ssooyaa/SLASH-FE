519a152e212a7ce6cb14bff573c5f90e65b462ae
김수지 <tvfxq0214@gmail.com>
2024-11-28 21:39:01 +0900

    백배포 테스트

--- src/api/Interceptor.js
+++ src/api/Interceptor.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://43.201.46.62:80",
   headers: {
     "Content-Type": "application/json",
   },
