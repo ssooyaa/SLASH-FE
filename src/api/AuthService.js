0e6f36d11ccd1d9ac5e102aff701af018ea848dc
김수지 <tvfxq0214@gmail.com>
2024-11-28 21:43:29 +0900

    로그인 에러메시지 테스트

--- src/api/AuthService.js
+++ src/api/AuthService.js
@@ -2,9 +2,11 @@ import axios from "./Interceptor";
 
 export const authLogin = async (id, password) => {
   try {
+
     const response = await axios.post("/login", { id, password });
     return response.data;
   } catch (error) {
+    console.error("ERROR: ", error.response.data);
     return { success: false, message: "로그인 실패. 다시 시도해주세요." };
   }
 };
