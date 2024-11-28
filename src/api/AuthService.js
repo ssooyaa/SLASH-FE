6b06f30bcfc18d84fea94eaedb1f239c81667ba1
김수지 <tvfxq0214@gmail.com>
2024-11-28 21:52:21 +0900

    Update AuthService.js

--- src/api/AuthService.js
+++ src/api/AuthService.js
@@ -2,11 +2,11 @@ import axios from "./Interceptor";
 
 export const authLogin = async (id, password) => {
   try {
-
+    console.log("로그인중");
     const response = await axios.post("/login", { id, password });
     return response.data;
   } catch (error) {
-    console.error("ERROR: ", error.response.data);
+    console.error(error);
     return { success: false, message: "로그인 실패. 다시 시도해주세요." };
   }
 };
